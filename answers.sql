c4b9bb33d5bb9f27f5f43df39a8fb3b8e67bf905
Jens Cox <jenscx8@gmail.com>
2023-07-31 09:36:25 -0600

    answered more just missinng the functions and a sql query i think

--- answers.sql
+++ answers.sql
@@ -26,5 +26,25 @@ select sum(num_cupcakes) from orders where processed = false;
 (1 row)
 
 //problem 4
+select c.name as name, coalesce(sum(case when o.cupcake_id is not null then o.num_cupcakes else 0 end), 0) as sum from cupcakes c left join orders o on c.id = cupcake_id group by c.name order by c.name asc;
+    name     | sum 
+-------------+-----
+ carrot cake |  10
+ chocolate   |   0
+ funfetti    |  13
+ raspberry   |  25
+ red velvet  | 215
+ vanilla     | 202
+(6 rows)
+
+// problem 5
+select c.email as email_address, coalesce(sum(o.num_cupcakes), 0) as total from customers c left join (select customer_id, sum(num_cupcakes) as num_cupcakes from orders group by customer_id) o on c.id = o.customer_id group by c.id, c.email order by total desc;
+     email_address      | total 
+------------------------+-------
+ ecrocker@gmail.com     |   265
+ hboyardee@yahoo.com    |   184
+ mcallender@hotmail.com |    16
+(3 rows)
+
 
 
